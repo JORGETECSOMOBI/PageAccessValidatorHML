6fc3733babdb937bf5082361e6acc2ba3d93466b
jorge.luiz@tecsomobi.com.br <cardoso2014.jc@gmail.com>
2023-10-27 09:14:33 -0300

    config retry

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://web.test.tmob.com.br",
-    retries: 0,
+    retries: 2,
 
     setupNodeEvents(on, config) {
       return config;
